b198fe6b6c6696a60f3a86600543f3a982f709dd
martin6724 <martin.michelleann@gmail.com>
2018-06-12 13:21:18 -0400

    adding more js, fixing bootstrap

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+  console.log('hi there!');
 $('.col-md-12').fadeIn("slow").removeClass('hidden');
-$('.col-md-12').animate({ top: "10px" }, 'slow');
+$('.col-md-12').animate({ bottom: "15px" }, 'slow');
 
-$('.homebuttons').animate({ top: "-500px" }, 3000);
-$('.homebuttons').fadeIn({queue: false, duration: 3000}).removeClass('hidden');
+// $('.homebuttons').show();
+// $('.hombuttons').animate({ opacity: 0 }, 0);
+$('.homebuttons').animate({ opacity: 1, top: "-10px"}, 'slow');
 });
